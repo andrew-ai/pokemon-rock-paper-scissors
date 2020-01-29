c3e37fa4cbe16b57e098a1d980d67980cb5b5284
andrew-ai <andrewnguyen.x@gmail.com>
2020-01-29 22:06:02 +1030

    added vue js

--- app.js
+++ app.js
@@ -1,22 +1,16 @@
-var cpu_hp = 5;
-var player_hp = 5;
+var app = new Vue({
+  el: '#screen',
+  data: {
+    cpu_hp: 5,
+    player_hp: 5,
+    move_state: 'r'
+  }
+})
 
-// r, p, s btns
-r_btn = document.getElementById("r");
-p_btn = document.getElementById("p");
-s_btn = document.getElementById("s");
 
 // cpu, player action imgs
 var cpu_action_img = document.getElementById("cpu-action-img");
 var player_action_img = document.getElementById("player-action-img");
-
-// hp bars
-player_hp_p = document.getElementById("player_hp");
-cpu_hp_p = document.getElementById("cpu_hp");
-
-r_btn.addEventListener("click", rMove);
-p_btn.addEventListener("click", pMove);
-s_btn.addEventListener("click", sMove);
 
 function rMove() {
   player_action_img.setAttribute("src", "images/rock.png");
@@ -128,17 +122,15 @@ function damageAnimate(elem_id) {
 
         // deduct damage
         if (elem_id=="cpu-img") {
-          cpu_hp = cpu_hp - 1;
-          cpu_hp_p.innerHTML = "HP: " + cpu_hp + "/5"
+          app.cpu_hp--;
         } else if (elem_id=="player-img") {
-          player_hp = player_hp - 1;
-          player_hp_p.innerHTML = "HP: " + player_hp + "/5"
+          app.player_hp--;
         }
 
         // winning sequence
-        if (cpu_hp == 0) {
+        if (app.cpu_hp == 0) {
           winAnimate("player");
-        } else if (player_hp == 0) {
+        } else if (app.player_hp == 0) {
           winAnimate("cpu");
         }
       } 
@@ -229,80 +221,58 @@ function animate(outcome) {
 /* KEY EVENTS */
 document.onkeydown = checkKey;
 
-move_state = 'r'
-
 function checkKey(e) {
 
     e = e || window.event;
 
     if (e.keyCode == '38') {
         // up arrow
-        if (move_state == 'r') {
-          updateMove('s');
-        }  else if (move_state == 'p') {
+        if (app.move_state == 'r') {
+          app.move_state = 's';
+        }  else if (app.move_state == 'p') {
 
-        } else if (move_state == 's') {
-          updateMove('r');
+        } else if (app.move_state == 's') {
+          app.move_state = 'r';
         }
     }
     else if (e.keyCode == '40') {
         // down arrow
-        if (move_state == 'r') {
-          updateMove('s');
-        }  else if (move_state == 'p') {
+        if (app.move_state == 'r') {
+          app.move_state = 's';
+        }  else if (app.move_state == 'p') {
           
-        } else if (move_state == 's') {
-          updateMove('r');
+        } else if (app.move_state == 's') {
+          app.move_state = 'r';
         }        
     }
     else if (e.keyCode == '37') {
        // left arrow
-       if (move_state == 'r') {
-        updateMove('p');
-      }  else if (move_state == 'p') {
-        updateMove('r');
-      } else if (move_state == 's') {
+       if (app.move_state == 'r') {
+        app.move_state = 'p';
+      }  else if (app.move_state == 'p') {
+        app.move_state = 'r';
+      } else if (app.move_state == 's') {
       }       
     }
     else if (e.keyCode == '39') {
        // right arrow
-       if (move_state == 'r') {
-        updateMove('p');
-      }  else if (move_state == 'p') {
-        updateMove('r');
-      } else if (move_state == 's') {
+       if (app.move_state == 'r') {
+        app.move_state = 'p';
+      }  else if (app.move_state == 'p') {
+        app.move_state = 'r';
+      } else if (app.move_state == 's') {
       }           
     }
     else if (e.keyCode == '13') {
       // enter
-      if (move_state == 'r') {
+      if (app.move_state == 'r') {
         rMove();
-      }  else if (move_state == 'p') {
+      }  else if (app.move_state == 'p') {
         pMove();
-      } else if (move_state == 's') {
+      } else if (app.move_state == 's') {
         sMove();
       }             
     }
 
-}
-
-function updateMove(new_move) {
-  // clear
-  r_btn = document.getElementById("r");
-  p_btn = document.getElementById("p");
-  s_btn = document.getElementById("s");
-  r_btn.innerHTML = 'ROCK';
-  p_btn.innerHTML = 'PAPER';
-  s_btn.innerHTML = 'SCISSORS';
-
-  if (new_move == 'r') {
-    r_btn.innerHTML = '<i class="fa fa-caret-right fa-lg"></i> ROCK';
-  } else if (new_move == 'p') {
-    p_btn.innerHTML = '<i class="fa fa-caret-right fa-lg"></i> PAPER';
-  } else if (new_move == 's') {
-    s_btn.innerHTML = '<i class="fa fa-caret-right fa-lg"></i> SCISSORS';
-  }
-
-  move_state = new_move;
 }
 
